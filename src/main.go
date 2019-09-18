522accdcc2f43fe4d85302c8cc58022c8839de8c
Lasse Nielsen <charmixer@users.noreply.github.com>
2019-09-18 11:10:02 +0200

    wip

--- src/main.go
+++ src/main.go
@@ -139,29 +139,13 @@ func createSecrets() {
   }
 }
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-const (
-    letterIdxBits = 6                    // 6 bits to represent a letter index
-    letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-    letterIdxMax  = 73 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
-
-func randStringBytesMask(n int) string {
-  b := make([]byte, n)
-  // A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
-  for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
-      if remain == 0 {
-          cache, remain = rand.Int63(), letterIdxMax
-      }
-      if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-          b[i] = letterBytes[idx]
-          i--
-      }
-      cache >>= letterIdxBits
-      remain--
+func randStringBytesMask(numberOfBytes int) (string) {
+  st := make([]byte, numberOfBytes)
+  _, err := rand.Read(st)
+  if err != nil {
+    return ""
   }
-
-  return string(b)
+  return base64.StdEncoding.EncodeToString(st)
 }
 
 func main() {
